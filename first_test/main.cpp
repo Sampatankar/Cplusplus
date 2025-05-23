4da02a8e45dc321eb11c9609484663ffddda1361
Sampatankar <50807100+Sampatankar@users.noreply.github.com>
2025-05-23 13:42:13 +0100

    Initial C++ Repo Initialisation

---
+++ first_test/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    // Declare variables to store the two numbers
+    int num1 = 5;
+    int num2 = 10;
+
+    // Calculate the sum of two numbers
+    int sum = num1 + num2;
+
+    // Display the result:
+    std::cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << std::endl;
+
+    // Return 0 to indicate succesful program execution
+    return 0;
+    
+}
---
+++ freecodecamp/ch2/firstcppprog/main2.cpp
@@ -0,0 +1,17 @@
+//This includes pre-built libraries
+#include <iostream>
+
+//Entry point of the main function:
+int main(){
+
+    std::cout << "Number1" << std::endl;
+    std::cout << "Number2" << std::endl;
+    std::cout << "Number3" << std::endl;
+
+    return 0;
+}
+
+/*
+This block of comments describes the output of this code:
+- the "NumberX" in brown are displayed on the command line, each on a line of their own.
+*/
